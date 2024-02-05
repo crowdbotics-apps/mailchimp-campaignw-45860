564177705e0906362a2f6a9d104c508a0316bae1
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:27:56 +0000

    Initial commit

---
+++ backend/mailchimp_campaignw_45860/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mailchimp_campaignw_45860 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mailchimp_campaignw_45860.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mailchimp-campaignw-45860",
+  url: "https://mailchimp-campaignw-45860.botics.co",
+  api: "https://mailchimp-campaignw-45860.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
